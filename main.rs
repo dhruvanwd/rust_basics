9f87fa61a059ff89526c1616ad5638704b1e3e8f
Sumit Kumar <33084149+dhruvanwd@users.noreply.github.com>
2024-05-31 17:25:04 +0000

    first commit

--- main.rs
+++ main.rs
@@ -1,3 +1,23 @@
-fn main(){
-    println!("Hello world")
+// fn main() {
+//     println!("Hello world");
+//     let a = {a:10};
+//     let b = {b:15};
+//     println!("{}", add(a, b));
+// }
+
+// fn add(num1: i32, num2: i32) -> i32 {
+//     return num1 + num2;
+// }
+
+
+
+fn main() {
+    println!("Hello world");
+    let a = (10,);
+    let b = (15,);
+    println!("{}", add(a, b));
+}
+
+fn add((num1,): (i32,), (num2,): (i32,)) -> i32 {
+    num1 + num2
 }
